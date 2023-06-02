7015221d93490468cab037b899eb3659e0432e8a
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-06-02 13:06:24 +0530

    Rust Scull Driver: Fileops device access

--- samples/rust/rust_scull.rs
+++ samples/rust/rust_scull.rs
@@ -1,8 +1,10 @@
 //! Rust Scull Driver
 
+use kernel::sync::{Arc, ArcBorrow, UniqueArc};
 use kernel::{prelude::*, file, miscdev};
 use kernel::file::File;
 use kernel::io_buffer::{IoBufferReader, IoBufferWriter};
+
 
 module! {
     type: Scull,
@@ -12,16 +14,23 @@ module! {
     license: "GPL",
 }
 
+#[derive(Debug)]
+struct Device {
+    num: usize,
+    contents: Vec<u8>,
+}
+
 struct Scull {
     _dev: Pin<Box<miscdev::Registration<Scull>>>,
 }
 
 #[vtable]
 impl file::Operations for Scull {
-
-    fn open(_context: &Self::OpenData, _file: &File) -> Result<Self::Data> {
-        pr_info!("File Opened\n");
-        Ok(())
+    type OpenData = Arc<Device>;
+    type Data = Arc<Device>;
+    fn open(context: &Self::OpenData, _file: &File) -> Result<Self::Data> {
+        pr_info!("File Opened for device {}\n", context.num);
+        Ok(context.clone())
     }
     
     fn release(_data: Self::Data, _file: &File) {
@@ -29,22 +38,22 @@ impl file::Operations for Scull {
     }
 
     fn read(
-        _data: (), 
+        data: ArcBorrow<'_, Device>, 
         _file: &file::File,
         _writer: &mut impl IoBufferWriter,
         _offset: u64,
     ) -> Result<usize> {
-        pr_info!("File is Read\n");
+        pr_info!("File being Read from {}\n", data.num);
         Ok(0)
     }
 
     fn write(
-        _data: (),
+        data: ArcBorrow<'_, Device>,
         _file: &file::File,
         reader: &mut impl IoBufferReader,
         _offset: u64,
     ) -> Result<usize> {
-        pr_info!("File is written\n");
+        pr_info!("File being Written {}\n", data.num);
         Ok(reader.len())
     }
 }
@@ -52,7 +61,12 @@ impl file::Operations for Scull {
 impl kernel::Module for Scull {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
         pr_info!("Scull Driver Loaded\n");
-        let reg = miscdev::Registration::new_pinned(fmt!("scull"), ())?;
+        let dev = Arc::try_new(Device{
+            num: 123,
+            contents: Vec::new(),
+        })?;
+
+        let reg = miscdev::Registration::new_pinned(fmt!("scull"), dev)?;
 
         Ok(Scull{
             _dev: reg
